1c80247f58a3c40afa4641a7f632632d3d81e8f8
matthieuharbonnier <matthieu.harbonnier@coorpacademy.com>
2016-02-19 16:41:28 +0100

    Project init

---
+++ web/index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<title>Swun.er • The swimrun training tracker</title>
+		<link href='https://fonts.googleapis.com/css?family=Lato:400,100,700' rel='stylesheet' type='text/css'>
+
+		<link rel="stylesheet" href="./css/main.css">
+		<link rel="icon" href="./img/favicon.png">
+
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+	</head>
+	<body>
+
+		Swun.er
+		
+	</body>
+</html>
