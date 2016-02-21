0f586e4b31fcb668465bea7d7c9eef3111afb98c
mattharbo <matthieu.harbonnier@gmail.com>
2016-02-21 15:56:13 +0100

    Header & footer creation

--- web/index.php
+++ web/index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 	<head>
 		<title>Swun.er • The swimrun training tracker</title>
-		<link href='https://fonts.googleapis.com/css?family=Lato:400,100,700' rel='stylesheet' type='text/css'>
+		<link href='https://fonts.googleapis.com/css?family=Lato:100,300,400,700' rel='stylesheet' type='text/css'>
 
 		<link rel="stylesheet" href="./css/main.css">
 		<link rel="icon" href="./img/favicon.png">
@@ -10,8 +10,13 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 	</head>
 	<body>
+		<div id="header">
+			<a href="#" class="main-logo"><img src="">Swun.er</a>
+			<a href="./settings" class="settings"></a>
+		</div><!-- Header -->
+		<div id="content">
 
-		Swun.er
-		
+		</div><!-- Content -->
+		<a href="./newsession" class="new-training-button">New training session</a>
 	</body>
 </html>
