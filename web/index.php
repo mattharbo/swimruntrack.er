d81feaa3270a17d5f072b01164271163042b4256
matthieuharbonnier <matthieu.harbonnier@coorpacademy.com>
2016-02-23 18:30:07 +0100

    Finishing items listing

--- web/index.php
+++ web/index.php
@@ -1,3 +1,12 @@
+<!--  
+ _____                         
+|   __|_ _ _ _ _ ___   ___ ___ 
+|__   | | | | | |   |_| -_|  _|
+|_____|_____|___|_|_|_|___|_|  
+ 
+ -->                              
+
+
 <!doctype html>
 <html lang="en">
 	<head>
@@ -23,17 +32,42 @@
 						<span class="date">23.02<span>
 					</div><!-- calendar -->
 				</div><!-- card-header -->
+
+				<!-- PHP LOOP HERE -->
 				<div id="card-content">
 					<div id="card-item">
 						<div class="circle"></div><!-- circle -->
-						<p>
+						<p class="record">
+							<p class="distance">600m </p>
+							<p class="time">14'32</p>
+							<p class="time-difference">(+2'57)</p>  
+						</p>
+					</div><!-- card-item -->	
+					<div id="card-item">
+						<div class="circle"></div><!-- circle -->
+						<p class="record">
+							<p class="distance">200m </p>
+							<p class="time">9'24</p>
+							<p class="time-difference">(+0'46)</p>  
 						</p>
 					</div><!-- card-item -->
 					<div id="card-item">
 						<div class="circle"></div><!-- circle -->
-						<p>
+						<p class="record">
+							<p class="distance">250m </p>
+							<p class="time">12'39</p>
+							<p class="time-difference">(+1'01)</p>  
 						</p>
 					</div><!-- card-item -->
+					<div id="card-item">
+						<div class="circle"></div><!-- circle -->
+						<p class="record">
+							<p class="distance">400m </p>
+							<p class="time">20'10</p>
+							<p class="time-difference">(+2'02)</p>  
+						</p>
+					</div><!-- card-item -->
+
 				</div><!-- card-content -->
 			</div><!-- last-card-session -->
 		</div><!-- Content -->
