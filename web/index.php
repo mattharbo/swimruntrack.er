9abaf1e4f427170a696fa0eeac1a1d4e0e464614
matthieuharbonnier <matthieu.harbonnier@coorpacademy.com>
2016-02-26 17:28:10 +0100

    Add Yandex Script

--- web/index.php
+++ web/index.php
@@ -17,8 +17,12 @@
 		<link rel="icon" href="./img/favicon.png">
 
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<!-- Yandex.Metrika counter --> <script type="text/javascript"> (function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter35497835 = new Ya.Metrika({ id:35497835, clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true }); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = "https://mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks"); </script> <noscript><div><img src="https://mc.yandex.ru/watch/35497835" style="position:absolute; left:-9999px;" alt="" /></div></noscript> <!-- /Yandex.Metrika counter -->
+		
 	</head>
 	<body>
+		<div id="testdiv">Matt</div>
 		<div id="header">
 			<a href="#" class="main-logo">Swun.er</a>
 			<a href="./settings" class="settings"></a>
